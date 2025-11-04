25b26ace585593731a4961efa91f2b55940e8800
Thomas Smith <hotpepper156@gmail.com>
2025-11-04 13:10:56 -0700

    Add contacts crud system

---
+++ lib/contacts_application/contacts.ex
@@ -0,0 +1,47 @@
+defmodule ContactsManager.Contacts do
+  @moduledoc """
+  The Contacts context module, handling all business logic
+  related to Contact records.
+  """
+  import Ecto.Query, warn: false
+  alias ContactsManager.Repo
+  alias ContactsManager.Contact
+
+  # --- Read Operations ---
+
+  @doc "Returns the list of all contacts."
+  def list_contacts do
+    Repo.all(Contact)
+  end
+
+  @doc "Gets a single contact by ID. Raises if not found."
+  def get_contact!(id) do
+    Repo.get!(Contact, id)
+  end
+
+  # --- Write Operations (CRUD) ---
+
+  @doc "Creates a new contact with the given attributes."
+  def create_contact(attrs) do
+    %Contact{}
+    |> Contact.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  @doc "Updates an existing contact with the given attributes."
+  def update_contact(%Contact{} = contact, attrs) do
+    contact
+    |> Contact.changeset(attrs)
+    |> Repo.update()
+  end
+
+  @doc "Deletes the given contact."
+  def delete_contact(%Contact{} = contact) do
+    Repo.delete(contact)
+  end
+
+  @doc "Returns an %Ecto.Changeset{} for form handling."
+  def change_contact(%Contact{} = contact, attrs \\ %{}) do
+    Contact.changeset(contact, attrs)
+  end
+end
